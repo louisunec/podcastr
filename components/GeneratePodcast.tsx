71b341ab7e7178e14cf6473265b472c7b083e797
louisunec <louisliu837@gmail.com>
2024-06-25 16:44:35 +1000

    first commit

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -3,14 +3,12 @@
 import { GeneratePodcastProps } from "@/types"
 import { Label } from "./ui/label"
 import { Textarea } from "./ui/textarea"
-import e from "express"
 import { Button } from "./ui/button"
 import { Loader } from "lucide-react"
 import { useState } from "react"
 import { useAction, useMutation } from "convex/react"
 import { api } from "@/convex/_generated/api"
 import { v4 as uuidv4 } from 'uuid'
-import { generateUploadUrl } from "@/convex/files"
 
 import { useUploadFiles } from "@xixixao/uploadstuff/react"
 import { useToast } from "@/components/ui/use-toast"
